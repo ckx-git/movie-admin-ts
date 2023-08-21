0653718763db1a5201bfd1cf505a7df90a10800d
changkaixin <dannyboy.vai.x@gmail.com>
2023-08-21 20:12:39 +0800

    客户端：搭建工程并完成ajax请求

---
+++ client/src/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { MovieService } from './services/MovieService';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+// MovieService.add({
+//   name: 'abc',
+//   timeLong: 33,
+//   types: ['喜剧'],
+//   areas: ['大陆'],
+//   isHot: false,
+//   isComing: false,
+//   isClassic: false
+// }).then(res => {
+//   if (res.err) {
+//     console.log(res.err)
+//   }
+// })
+
+MovieService.getMovies({}).then(movies => {
+  movies.data.forEach(m => {
+    console.log(m.name, m.types)
+  })
+})
