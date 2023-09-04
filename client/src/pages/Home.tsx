e0b90df50b1588167784be9c4dc65445495ffd7b
changkaixin <dannyboy.vai.x@gmail.com>
2023-09-04 18:49:41 +0800

    客户端：制作图片上传组件、制作电影表单组件

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import React from "react"
+import ImgUploader from "../components/ImgUploader";
 
 export default class extends React.Component {
-    render(){
+
+    render() {
         return (
             <h1>
                 欢迎使用电影管理系统
